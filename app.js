a7b81421e596bb188aa8b8d0543d6e4a49836d2e
Dinesh Kumar (C) <dinesh_budhiraja@yahoo.com>
2024-05-01 11:35:06 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//create a new repo
+// add .readme .gitignore files
+// copy the url
+// git clone https://github.com/username/repo.git 
+// open the project in code (cd repofolder)
+// npm init -y
+// add new file app.js with some console statements
+// npm i 
+// git add .
+// git commit -m "message to add file"
+// git push
+console.log("app.js file for node");
+console.log(new Date());

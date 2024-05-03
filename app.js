7cdef4bec4685cf7398145c2cd05edaeceef0bd2
Dinesh Kumar (C) <dinesh_budhiraja@yahoo.com>
2024-05-03 11:51:06 +0530

    added express, actions configured for self-hosted, created api in app.js

--- app.js
+++ app.js
@@ -13,4 +13,11 @@ console.log("app.js file for node");
 console.log(new Date());
 console.log("added line 3");
 console.log("edited in browser");
+const app = require('express')();
+app.get("/", (req,res) => {
+    res.json({message : "Server running"})
+})
+app.listen(7000, () => {
+    console.log("server running on 7000");
+})
 
